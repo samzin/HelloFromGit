f3ef47149c7cd42e5973d85b1ee8ac100a3a66c6
samzin <zinjadesamir77@gmail.com>
2018-07-24 01:07:40 +0530

    java

--- app/src/main/java/com/oort/myapplication/MainActivity.java
+++ app/src/main/java/com/oort/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.oort.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "testing for vcs", Toast.LENGTH_SHORT).show();
     }
 }
